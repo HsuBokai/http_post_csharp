b17f734bbf132a3a7249d5b998c321f99f518929
HsuBokai <exp2.718281@gmail.com>
2015-05-06 22:11:31 +0800

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.media_path = new System.Windows.Forms.TextBox();
+            this.import_medias = new System.Windows.Forms.Button();
+            this.term_file = new System.Windows.Forms.TextBox();
+            this.select_term_file = new System.Windows.Forms.Button();
+            this.search_btn = new System.Windows.Forms.Button();
+            this.select_output_path = new System.Windows.Forms.Button();
+            this.output_path = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.clear_list_box = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(582, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // media_path
+            // 
+            this.media_path.Location = new System.Drawing.Point(125, 21);
+            this.media_path.Name = "media_path";
+            this.media_path.Size = new System.Drawing.Size(451, 27);
+            this.media_path.TabIndex = 1;
+            // 
+            // import_medias
+            // 
+            this.import_medias.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.import_medias.Location = new System.Drawing.Point(620, 22);
+            this.import_medias.Name = "import_medias";
+            this.import_medias.Size = new System.Drawing.Size(98, 23);
+            this.import_medias.TabIndex = 2;
+            this.import_medias.Text = "GenMetadata";
+            this.import_medias.UseVisualStyleBackColor = true;
+            this.import_medias.Click += new System.EventHandler(this.import_medias_Click);
+            // 
+            // term_file
+            // 
+            this.term_file.Location = new System.Drawing.Point(125, 21);
+            this.term_file.Name = "term_file";
+            this.term_file.Size = new System.Drawing.Size(451, 27);
+            this.term_file.TabIndex = 3;
+            // 
+            // select_term_file
+            // 
+            this.select_term_file.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.select_term_file.Location = new System.Drawing.Point(582, 19);
+            this.select_term_file.Name = "select_term_file";
+            this.select_term_file.Size = new System.Drawing.Size(32, 23);
+            this.select_term_file.TabIndex = 4;
+            this.select_term_file.Text = "...";
+            this.select_term_file.UseVisualStyleBackColor = true;
+            this.select_term_file.Click += new System.EventHandler(this.select_term_file_Click);
+            // 
+            // search_btn
+            // 
+            this.search_btn.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.search_btn.Location = new System.Drawing.Point(620, 61);
+            this.search_btn.Name = "search_btn";
+            this.search_btn.Size = new System.Drawing.Size(98, 23);
+            this.search_btn.TabIndex = 5;
+            this.search_btn.Text = "Search";
+            this.search_btn.UseVisualStyleBackColor = true;
+            this.search_btn.Click += new System.EventHandler(this.search_btn_Click);
+            // 
+            // select_output_path
+            // 
+            this.select_output_path.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.select_output_path.Location = new System.Drawing.Point(582, 61);
+            this.select_output_path.Name = "select_output_path";
+            this.select_output_path.Size = new System.Drawing.Size(32, 23);
+            this.select_output_path.TabIndex = 6;
+            this.select_output_path.Text = "...";
+            this.select_output_path.UseVisualStyleBackColor = true;
+            this.select_output_path.Click += new System.EventHandler(this.select_output_path_Click);
+            // 
+            // output_path
+            // 
+            this.output_path.Location = new System.Drawing.Point(125, 58);
+            this.output_path.Name = "output_path";
+            this.output_path.Size = new System.Drawing.Size(451, 27);
+            this.output_path.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.media_path);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.import_medias);
+            this.groupBox1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(21, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(739, 63);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Import Media Files";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Media Folder :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.term_file);
+            this.groupBox2.Controls.Add(this.select_term_file);
+            this.groupBox2.Controls.Add(this.output_path);
+            this.groupBox2.Controls.Add(this.search_btn);
+            this.groupBox2.Controls.Add(this.select_output_path);
+            this.groupBox2.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox2.Location = new System.Drawing.Point(21, 90);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(739, 95);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(6, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Term Set File :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(6, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Output Folder :";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(21, 204);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(614, 112);
+            this.listBox1.TabIndex = 13;
+            // 
+            // clear_list_box
+            // 
+            this.clear_list_box.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.clear_list_box.Location = new System.Drawing.Point(641, 204);
+            this.clear_list_box.Name = "clear_list_box";
+            this.clear_list_box.Size = new System.Drawing.Size(98, 23);
+            this.clear_list_box.TabIndex = 13;
+            this.clear_list_box.Text = "Clear";
+            this.clear_list_box.UseVisualStyleBackColor = true;
+            this.clear_list_box.Click += new System.EventHandler(this.clear_list_box_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 328);
+            this.Controls.Add(this.clear_list_box);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox media_path;
+        private System.Windows.Forms.Button import_medias;
+        private System.Windows.Forms.TextBox term_file;
+        private System.Windows.Forms.Button select_term_file;
+        private System.Windows.Forms.Button search_btn;
+        private System.Windows.Forms.Button select_output_path;
+        private System.Windows.Forms.TextBox output_path;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button clear_list_box;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.media_path.Text = this.folderBrowserDialog1.SelectedPath;
+            }   
+        }
+
+        private void import_medias_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(this.media_path.Text)) return;
+            DirectoryInfo di = new DirectoryInfo(this.media_path.Text);
+            try
+            {
+                foreach (var fi in di.EnumerateFiles())
+                {
+                    if(fi.Extension == ".wav"){
+                        
+                        XmlDocument xml_doc = new XmlDocument();
+                        XmlElement testdata = xml_doc.CreateElement("TestData");
+                        xml_doc.AppendChild(testdata);
+                        XmlElement externalMediaId = xml_doc.CreateElement("ExternalMediaId");
+                        externalMediaId.InnerText = fi.Name;
+                        testdata.AppendChild(externalMediaId);
+                        xml_doc.Save(fi.FullName + ".xml");
+                        this.listBox1.Items.Add("Generate " + fi.FullName + ".xml");
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException DirNotFound) {
+                this.listBox1.Items.Add(DirNotFound.Message);
+            }
+            catch (UnauthorizedAccessException UnAuthDir) {
+                this.listBox1.Items.Add(UnAuthDir.Message);
+            }
+            catch (PathTooLongException LongPath) {
+                this.listBox1.Items.Add(LongPath.Message);
+            }
+        }
+
+        private void select_term_file_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.term_file.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void search_btn_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(this.media_path.Text)) return;
+            if (!File.Exists(this.term_file.Text)) return;
+            if (!Directory.Exists(this.output_path.Text)) return;
+            
+            XmlDocument doc = new XmlDocument();
+            XmlElement searchRequest = doc.CreateElement("SearchRequest");
+            doc.AppendChild(searchRequest);
+            XmlElement expression = doc.CreateElement("Expression");
+
+            XmlElement or_tag = doc.CreateElement("Or");
+            String[] terms = File.ReadAllText(this.term_file.Text).Split('\n');
+            foreach (var term in terms)
+            {
+                //Console.WriteLine("{0}", term);
+                char[] separators = { ',', '"' };
+                String[] components = term.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                if(components.Length >= 3){
+                    XmlElement phrase = doc.CreateElement("Phrase");
+                    phrase.SetAttribute("Threshold", components[1]);
+                    phrase.InnerText = components[0];
+                    or_tag.AppendChild(phrase);
+                }
+            }
+            expression.AppendChild(or_tag);
+
+            searchRequest.AppendChild(expression);
+            XmlElement languageId = doc.CreateElement("LanguageId");
+            languageId.InnerText = "e105eaea-9005-4ee6-9cbd-156ee2d3286e";
+            searchRequest.AppendChild(languageId);
+            XmlElement threshold = doc.CreateElement("Threshold");
+            threshold.InnerText = "70";
+            searchRequest.AppendChild(threshold);
+            XmlElement maxResults = doc.CreateElement("MaxResults");
+            maxResults.InnerText = "100";
+            searchRequest.AppendChild(maxResults);
+            XmlElement externalMediaIds = doc.CreateElement("ExternalMediaIds");
+
+            DirectoryInfo di = new DirectoryInfo(this.media_path.Text);
+            try
+            {
+                foreach (var fi in di.EnumerateFiles())
+                {
+                    if (fi.Extension == ".wav")
+                    {
+                        XmlElement externalMediaId = doc.CreateElement("ExternalMediaId");
+                        externalMediaId.InnerText = fi.Name;
+                        externalMediaIds.AppendChild(externalMediaId);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException DirNotFound)
+            {
+                Console.WriteLine("(0)", DirNotFound.Message);
+            }
+            catch (UnauthorizedAccessException UnAuthDir)
+            {
+                Console.WriteLine("(0)", UnAuthDir.Message);
+            }
+            catch (PathTooLongException LongPath)
+            {
+                Console.WriteLine("(0)", LongPath.Message);
+            }
+
+            searchRequest.AppendChild(externalMediaIds);
+
+            string search_xml_file = this.output_path.Text + "/search.xml";
+            string result_file = this.output_path.Text + "/result.xml";
+            doc.Save(search_xml_file);
+
+            this.listBox1.Items.Add("Search: " + search_xml_file);
+            this.Enabled = false;
+            //Console.WriteLine("{0}", File.ReadAllText(search_xml_file));
+            string res_xml = HTTP_POST("http://localhost:8080/audiofinder/search", File.ReadAllText(search_xml_file));
+            File.WriteAllText(result_file, res_xml);
+            this.Enabled = true;
+            MessageBox.Show("Search finished");
+            this.listBox1.Items.Add("Result: " + result_file);
+        }
+
+        private void select_output_path_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                this.output_path.Text = this.folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+
+        public static string HTTP_POST(string Url, string Data)
+        {
+            string Out = String.Empty;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(Url);
+            try
+            {
+                req.Method = "POST";
+                req.Timeout = 100000;
+                req.ContentType = "application/xml";
+                req.Accept = "application/xml";
+
+                byte[] sentData = Encoding.UTF8.GetBytes(Data);
+                req.ContentLength = sentData.Length;
+                using (System.IO.Stream sendStream = req.GetRequestStream())
+                {
+                    sendStream.Write(sentData, 0, sentData.Length);
+                    sendStream.Close();
+                }
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                System.IO.Stream ReceiveStream = res.GetResponseStream();
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8))
+                {
+                    Char[] read = new Char[256];
+                    int count = sr.Read(read, 0, 256);
+
+                    while (count > 0)
+                    {
+                        String str = new String(read, 0, count);
+                        Out += str;
+                        count = sr.Read(read, 0, 256);
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Out = string.Format("HTTP_ERROR :: The second HttpWebRequest object has raised an Argument Exception as 'Connection' Property is set to 'Close' :: {0}", ex.Message);
+            }
+            catch (WebException ex)
+            {
+                Out = string.Format("HTTP_ERROR :: WebException raised! :: {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Out = string.Format("HTTP_ERROR :: Exception raised! :: {0}", ex.Message);
+            }
+
+            return Out;
+        }
+
+        /// <summary>
+        /// Sending GET request.
+        /// </summary>
+        /// <param name="Url">Request Url.</param>
+        /// <param name="Data">Data for request.</param>
+        /// <returns>Response body.</returns>
+        public static string HTTP_GET(string Url, string Data)
+        {
+            string Out = String.Empty;
+            WebRequest req = WebRequest.Create(Url + (string.IsNullOrEmpty(Data) ? "" : "?" + Data));
+            try
+            {
+                WebResponse resp = req.GetResponse();
+                using (System.IO.Stream stream = resp.GetResponseStream())
+                {
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(stream))
+                    {
+                        Out = sr.ReadToEnd();
+                        sr.Close();
+                    }
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Out = string.Format("HTTP_ERROR :: The second HttpWebRequest object has raised an Argument Exception as 'Connection' Property is set to 'Close' :: {0}", ex.Message);
+            }
+            catch (WebException ex)
+            {
+                Out = string.Format("HTTP_ERROR :: WebException raised! :: {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Out = string.Format("HTTP_ERROR :: Exception raised! :: {0}", ex.Message);
+            }
+
+            return Out;
+        }
+
+        private void clear_list_box_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+        }
+        
+    }
+}
